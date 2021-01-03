6183b726185bd5666b5279e7d721af2d834033f9
Sarvagna Malladi <sarvagna.malladi@gmail.com>
2021-01-02 22:08:44 -0500

    Add project files.

---
+++ SocialMediaApp/SocialMediaApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SocialMediaApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SocialMediaApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
