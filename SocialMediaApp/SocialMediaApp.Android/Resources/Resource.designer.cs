92c40402bd008cc6465f9797538d1ff987d8b884
Sarvagna Malladi <sarvagna.malladi@gmail.com>
2021-01-30 14:50:43 -0500

    added formatting

--- SocialMediaApp/SocialMediaApp.Android/Resources/Resource.designer.cs
+++ SocialMediaApp/SocialMediaApp.Android/Resources/Resource.designer.cs
@@ -8400,13 +8400,16 @@ namespace SocialMediaApp.Droid
 			public const int pfp = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tooltip_frame_dark = 2131165307;
+			public const int pfpwhite = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tooltip_frame_light = 2131165308;
+			public const int tooltip_frame_dark = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int xamarin_logo = 2131165309;
+			public const int tooltip_frame_light = 2131165309;
+			
+			// aapt resource value: 0x7F07007E
+			public const int xamarin_logo = 2131165310;
 			
 			static Drawable()
 			{
